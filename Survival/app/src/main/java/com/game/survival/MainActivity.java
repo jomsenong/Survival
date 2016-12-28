875ef346e7cff27002bb37c8c8e9c10a7dd80e65
Jeremy <jomsenong@hotmail.com>
2016-12-28 21:03:40 +0800

    Deep Survival
    Start up screen with new game, resume and cancel onclick. Exit available
    as well

---
+++ Survival/app/src/main/java/com/game/survival/MainActivity.java
@@ -0,0 +1,62 @@
+package com.game.survival;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+
+        b1 = (Button)findViewById(R.id.button);
+        b2 = (Button)findViewById(R.id.button2);
+
+        //final Intent intent = new Intent(this, Mainactivity.class);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Resume or Start a New Game ");
+                builder.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked New Game button
+                        dialog.cancel();
+                    }
+                });
+                builder.setNeutralButton("New Game", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked Cancel button
+                        Toast.makeText(getApplicationContext(), "Populating a new map...",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("Resume", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked the Resume button
+                        Toast.makeText(getApplicationContext(), "Resuming...",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
