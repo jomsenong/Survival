1f81b79453d27863b818089b4320ead7daf9e95b
Jeremy <jomsenong@hotmail.com>
2016-12-29 22:48:02 +0800

    Game layout
    Added gridlayout for game

---
+++ Survival/app/src/main/java/com/game/survival/Game.java
@@ -0,0 +1,16 @@
+package com.game.survival;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ong on 28/12/2016.
+ */
+
+public class Game extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    }
+}
